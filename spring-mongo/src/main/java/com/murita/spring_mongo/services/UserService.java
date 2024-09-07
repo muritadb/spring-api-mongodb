df5ee7935b096260a1a8e48279e43f6d59d78e96
Douglas Murita <douglasb008@gmail.com>
2024-09-07 13:10:51 -0300

    feat: add update method user

--- spring-mongo/src/main/java/com/murita/spring_mongo/services/UserService.java
+++ spring-mongo/src/main/java/com/murita/spring_mongo/services/UserService.java
@@ -27,7 +27,6 @@ public class UserService {
     return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado!"));
   }
 
-  // post de um user
   public User insert(User obj) {
     return repo.insert(obj);
   }
@@ -35,6 +34,17 @@ public class UserService {
   public void delete(String id) {
     findById(id);
     repo.deleteById(id);
+  }
+
+  public User update(User obj) {
+    User newObj = findById(obj.getId());
+    updateData(newObj, obj);
+    return repo.save(newObj);
+  }
+
+  private void updateData(User newObj, User obj) {
+    newObj.setName(obj.getName());
+    newObj.setEmail(obj.getEmail());
   }
 
   public User fromDTO(UserDTO objDto) {
