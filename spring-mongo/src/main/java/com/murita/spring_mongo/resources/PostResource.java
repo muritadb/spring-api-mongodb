b886d4765854962b1ff0c290612ca5a70e713161
Douglas Murita <douglasb008@gmail.com>
2024-09-18 23:28:57 -0300

    feat: add query multi fields

--- spring-mongo/src/main/java/com/murita/spring_mongo/resources/PostResource.java
+++ spring-mongo/src/main/java/com/murita/spring_mongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.murita.spring_mongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,4 +37,19 @@ public class PostResource {
 
     return ResponseEntity.ok().body(list);
   }
+
+  @GetMapping("/fullsearch")
+  public ResponseEntity<List<Post>> fullSearch(
+      @RequestParam(value = "text", defaultValue = "") String text,
+      @RequestParam(value = "minDate", defaultValue = "") String minDate,
+      @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+    text = URL.decodeParam(text);
+    Date min = URL.convertDate(minDate, new Date(0L));
+    Date max = URL.convertDate(maxDate, new Date());
+    List<Post> list = postService.fullSearch(text, min, max);
+
+    return ResponseEntity.ok().body(list);
+  }
+
 }
